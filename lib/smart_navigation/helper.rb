7d527f43068b6090c576a3d1d8e4d21ab1f3d8f4
Jonian Guveli <jonian@hardpixel.eu>
2019-01-07 13:28:22 +0200

    remove useless comments

--- lib/smart_navigation/helper.rb
+++ lib/smart_navigation/helper.rb
@@ -1,11 +1,9 @@
 module SmartNavigation
   module Helper
-    # Render navigation builder
     def smart_navigation_for(items, options = {})
       SmartNavigation::Renderer.new(self, items, options).render
     end
 
-    # Alias helper method
     alias :navigation_for :smart_navigation_for
   end
 end
--- lib/smart_navigation/renderer.rb
+++ lib/smart_navigation/renderer.rb
@@ -1,20 +1,17 @@
 module SmartNavigation
   class Renderer
-    # Initialize builder
     def initialize(view_context, items, options = {})
       @context = view_context
       @items   = sort_items items
       @options = merge_options options
     end
 
-    # Render menu
     def render
       menu_tag @items.map { |k, v| item_tag(v, @options[:menu_icons]) }.join
     end
 
     private
 
-    # Default options
     def default_options
       {
         menu_class:           'menu',
@@ -36,7 +33,6 @@ module SmartNavigation
       }
     end
 
-    # Get merged options
     def merge_options(options)
       if options[:keep_defaults] == false
         options
@@ -45,12 +41,10 @@ module SmartNavigation
       end
     end
 
-    # Sort items by order
     def sort_items(items)
       items.sort_by { |_k, v| v[:order] }.to_h
     end
 
-    # Check if url parent
     def page_parent?(item)
       url   = item_url item
       paths = [@context.request.path, @context.request.url]
@@ -58,7 +52,6 @@ module SmartNavigation
       url.present? && item[:root].blank? && paths.any? { |i| i.starts_with?(url) }
     end
 
-    # Get menu item url
     def item_url(item)
       if item[:url].present?
         mixed_value(item[:url])
@@ -67,7 +60,6 @@ module SmartNavigation
       end
     end
 
-    # Check if should render item
     def render_item?(item)
       if item[:if]
         mixed_value(item[:if]).present?
@@ -78,7 +70,6 @@ module SmartNavigation
       end
     end
 
-    # Check if current page
     def current_page?(item)
       current = @context.current_page?(item_url(item))
       current = page_parent?(item) if item[:children].blank? and current.blank?
@@ -86,7 +77,6 @@ module SmartNavigation
       current
     end
 
-    # Check if current group
     def current_group?(item)
       current = current_page?(item)
       current = Hash(item[:children]).any? { |_k, v| current_page?(v) } if current.blank?
@@ -95,12 +85,10 @@ module SmartNavigation
       current
     end
 
-    # Create html tag
     def tag(*args, &block)
       @context.content_tag(*args, &block)
     end
 
-    # Create menu icon
     def icon_tag(name, label = nil)
       icon = tag :i, nil, class: "#{@options[:icon_prefix]}#{name || @options[:icon_default]}"
 
@@ -111,17 +99,14 @@ module SmartNavigation
       end
     end
 
-    # Create submenu toggle tag
     def toggle_tag
       "#{@options[:submenu_toggle]}".html_safe
     end
 
-    # Create menu separator
     def separator_tag(item)
       tag :li, item[:label], class: @options[:separator_class]
     end
 
-    # Create item link
     def item_link_tag(item, icons = false)
       label = tag :span, item[:label]
       label = icon_tag("#{item[:icon]}", label) if icons.present?
@@ -135,7 +120,6 @@ module SmartNavigation
       end
     end
 
-    # Create submenu item
     def submenu_item_tag(item, active = false)
       items  = sort_items item[:children]
       items  = items.map { |_k, v| item_tag(v, @options[:submenu_icons], true) }.join
@@ -144,7 +128,6 @@ module SmartNavigation
       tag(:ul, items.html_safe, class: "#{active} #{@options[:submenu_class]}".strip)
     end
 
-    # Create group menu item
     def group_item_tag(item, icons = false)
       active  = @options[:active_class] if current_group?(item)
       link    = item_link_tag item, icons
@@ -154,7 +137,6 @@ module SmartNavigation
       tag :li, content.html_safe, class: "#{active} #{@options[:submenu_parent_class]}".strip
     end
 
-    # Create single menu item
     def single_item_tag(item, icons = false, subitem = false)
       active = @options[:active_class] if current_page?(item)
       iclass = subitem ? @options[:submenu_item_class] : @options[:item_class]
@@ -165,7 +147,6 @@ module SmartNavigation
       tag :li, link.html_safe, opts
     end
 
-    # Create menu list item
     def item_tag(item, icons = false, subitem = false)
       if render_item?(item)
         if item[:separator].present?
@@ -178,12 +159,10 @@ module SmartNavigation
       end
     end
 
-    # Create menu list
     def menu_tag(items)
       tag :ul, items.html_safe, Hash(@options[:menu_html]).merge(class: @options[:menu_class])
     end
 
-    # Parse mixed value
     def mixed_value(value)
       if value.is_a?(Proc)
         @context.instance_exec(&value)
